4c2643ab69de4d12c6f8193c0636c97242629391
Nikola Crhák <122156533+crhaxx@users.noreply.github.com>
2026-03-08 00:10:34 +0100

    Tailwind migrate

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,25 @@ import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./providers/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import { ProductsProvider } from "./providers/ProductsProvider";
+import "./index.css";
 
+console.log("APP RENDER");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthProvider>
       <ProductsProvider>
         <Toaster
-        position="top-right"
-        toastOptions={{
-          style: {
-            background: "#111",
-            color: "white",
-            border: "1px solid #333",
-          },
-        }}
-      />
-      <AppRoutes />
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: "#111",
+              color: "white",
+              border: "1px solid #333",
+            },
+          }}
+        />
+        <AppRoutes />
       </ProductsProvider>
     </AuthProvider>
+
 )
